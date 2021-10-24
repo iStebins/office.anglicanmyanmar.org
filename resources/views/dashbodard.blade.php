4c00d1ecd08d3983b70091004083d87d1501f441
sastephen <hello@sastephen.vip>
2021-10-24 22:39:25 +0630

    update dashboard ui

--- resources/views/dashbodard.blade.php
+++ resources/views/dashbodard.blade.php
@@ -16,7 +16,7 @@
     <div class="row">
 
         <!-- Left side columns -->
-        <div class="col-lg-8">
+        <div class="col-lg-8 col-sm-12">
             <div class="row">
 
                 <!-- Dioceses -->
@@ -58,112 +58,51 @@
                 @endforeach
                 <!-- End Dioceses -->
 
-                <!-- Reports -->
-                <div class="col-12 row">
-                    <div class="col-9">
-                      <!-- Recent Activity -->
-                    <div class="card">
-                      <div class="card-body">
-                          <h5 class="card-title">Ethnicities</h5>
+                <div class="row">
+                    {{-- left --}}
+                    <div class="col-lg-4 col-sm-12">
 
-                          <!-- Pie Chart -->
-                          <div id="pieChart" style="min-height: 400px;" class="echart"></div>
-
-                          <script>
-                              document.addEventListener("DOMContentLoaded", () => {
-                                  echarts.init(document.querySelector("#pieChart")).setOption({
-                                      // title: {
-                                      //     text: 'Ethnicities',
-                                      //     subtext: 'Fake Data',
-                                      //     left: 'center'
-                                      // },
-                                      tooltip: {
-                                          trigger: 'item'
-                                      },
-                                      legend: {
-                                          orient: 'vertical',
-                                          left: 'left'
-                                      },
-                                      series: [{
-                                          name: 'Ethnicity',
-                                          type: 'pie',
-                                          radius: '50%',
-                                          data: [
-                                              @foreach($ethnicities as $ethinicity) {
-                                                  value: 735,
-                                                  name: "{{ $ethinicity->name }}"
-                                              },
-                                              @endforeach
-                                          ],
-                                          emphasis: {
-                                              itemStyle: {
-                                                  shadowBlur: 10,
-                                                  shadowOffsetX: 0,
-                                                  shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                              }
-                                          }
-                                      }]
-                                  });
-                              });
-
-                          </script>
-                          <!-- End Pie Chart -->
-
-                      </div>
-                  </div>
-                    </div>
-
-                    <div class="col-3">
-                      <!-- Recent Activity -->
-                    <div class="card">
-                      <div class="filter">
-                        <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                          <li class="dropdown-header text-start">
-                            <h6>Filter</h6>
-                          </li>
-          
-                          <li><a class="dropdown-item" href="#">Today</a></li>
-                          <li><a class="dropdown-item" href="#">This Month</a></li>
-                          <li><a class="dropdown-item" href="#">This Year</a></li>
-                        </ul>
-                      </div>
-          
-                      <div class="card-body">
-                        <h5 class="card-title">Magazines <span>| Latest</span></h5>
-          
-                        <div class="activity">
-          
-                          @foreach ($magazines as $magazine)
-                          <div class="activity-item d-flex">
-                            <div class="activite-label"><a target="_" href="{{ $magazine->file ? asset("documents/$magazine->file") : '#' }}">{{ $magazine->edition }} / {{ $magazine->issue }}</a></div>
-                            <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
-                            <div class="activity-content">
-                              {{-- Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae --}}
-                              {{ $magazine->date }}
+                        <div class="card" id="magazines">
+                            <div class="filter">
+                                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                    <li class="dropdown-header text-start">
+                                        <h6>Filter</h6>
+                                    </li>
+                    
+                                    <li><a class="dropdown-item" href="#">Today</a></li>
+                                    <li><a class="dropdown-item" href="#">This Month</a></li>
+                                    <li><a class="dropdown-item" href="#">This Year</a></li>
+                                </ul>
                             </div>
-                          </div>
-                          @endforeach
-                          <!-- End item-->
-          
+                    
+                            <div class="card-body">
+                                <h5 class="card-title">Magazines <span>| Latest</span></h5>
+                    
+                                <div class="activity">
+                    
+                                    @foreach ($magazines as $magazine)
+                                    <div class="activity-item d-flex">
+                                        <div class="activite-label"><a target="_"
+                                                href="{{ $magazine->file ? asset("documents/$magazine->file") : '#' }}">{{ $magazine->edition }}
+                                                / {{ $magazine->issue }}</a></div>
+                                        <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
+                                        <div class="activity-content">
+                                            {{-- Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae --}}
+                                            {{ $magazine->date }}
+                                        </div>
+                                    </div>
+                                    @endforeach
+                                    <!-- End item-->
+                                </div>
+                            </div>
                         </div>
-          
-                      </div>
-                    </div><!-- End Recent Activity -->
-                    </div>
-                </div>
-
-
-
-
-                <!-- Top Selling -->
-                <div class="col-12 row">
-
-                    <div class="col-4">
-                        <div class="card">
+                        {{-- End Magazines --}}
+                    
+                        <div class="card" id="departments">
                             <div class="card-body">
                                 <h5 class="card-title">Departments</h5>
-
+                    
                                 <!-- Table with stripped rows -->
                                 <table class="table table-striped">
                                     <thead>
@@ -182,32 +121,84 @@
                                     </tbody>
                                 </table>
                                 <!-- End Table with stripped rows -->
-
+                    
                             </div>
                         </div>
+                        {{-- End Departments --}}
+
                     </div>
 
-                    <!-- Male | Female -->
-                    <div class="col-8">
-                        <div class="card">
+                    {{-- center --}}
+                    <div class="col-lg-8 col-sm-12">
+                        <div class="card" id="ethnicities">
+                            <div class="card-body">
+                                <h5 class="card-title">Ethnicities</h5>
+                    
+                                <!-- Pie Chart -->
+                                <div id="pieChart" style="min-height: 400px;" class="echart"></div>
+                    
+                                <script>
+                                    document.addEventListener("DOMContentLoaded", () => {
+                                        echarts.init(document.querySelector("#pieChart")).setOption({
+                                            // title: {
+                                            //     text: 'Ethnicities',
+                                            //     subtext: 'Fake Data',
+                                            //     left: 'center'
+                                            // },
+                                            tooltip: {
+                                                trigger: 'item'
+                                            },
+                                            legend: {
+                                                orient: 'vertical',
+                                                left: 'left'
+                                            },
+                                            series: [{
+                                                name: 'Ethnicity',
+                                                type: 'pie',
+                                                radius: '50%',
+                                                data: [
+                                                    @foreach($ethnicities as $ethinicity) {
+                                                        value: 735,
+                                                        name: "{{ $ethinicity->name }}"
+                                                    },
+                                                    @endforeach
+                                                ],
+                                                emphasis: {
+                                                    itemStyle: {
+                                                        shadowBlur: 10,
+                                                        shadowOffsetX: 0,
+                                                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                                    }
+                                                }
+                                            }]
+                                        });
+                                    });
+                    
+                                </script>
+                                <!-- End Pie Chart -->
+                            </div>
+                        </div>
+                        {{-- End Ethnicities --}}
+                    
+                        <div class="card" id="male-female">
                             <div class="filter">
                                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                                     <li class="dropdown-header text-start">
                                         <h6>Filter</h6>
                                     </li>
-
+                    
                                     <li><a class="dropdown-item" href="#">Today</a></li>
                                     <li><a class="dropdown-item" href="#">This Month</a></li>
                                     <li><a class="dropdown-item" href="#">This Year</a></li>
                                 </ul>
                             </div>
-
+                    
                             <div class="card-body pb-0">
                                 <h5 class="card-title">Male | Female</h5>
-
+                    
                                 <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
-
+                    
                                 <script>
                                     document.addEventListener("DOMContentLoaded", () => {
                                         echarts.init(document.querySelector("#trafficChart")).setOption({
@@ -249,44 +240,44 @@
                                             }]
                                         });
                                     });
-
+                    
                                 </script>
-
+                    
                             </div>
                         </div>
+                        {{-- End Male | Female --}}
                     </div>
-                    <!-- End Male | Female -->
-
-                </div><!-- End Top Selling -->
+                </div>
 
             </div>
+
+
         </div><!-- End Left side columns -->
 
         <!-- Right side columns -->
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-12">
 
-            <div class="card">
+            <div class="card" id="news">
                 <div class="filter">
                     <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                         <li class="dropdown-header text-start">
                             <h6>Filter</h6>
                         </li>
-
+        
                         <li><a class="dropdown-item" href="#">Today</a></li>
                         <li><a class="dropdown-item" href="#">This Month</a></li>
                         <li><a class="dropdown-item" href="#">This Year</a></li>
                     </ul>
                 </div>
-
+        
                 <div class="card-body pb-0">
                     <h5 class="card-title">News <span>| Latest</span></h5>
-
+        
                     <div class="news">
                         @foreach ($news as $new)
                         <div class="post-item clearfix">
-                            <img src="{{ $new->image ? asset("images/$new->image") : asset("assets/img/logo.png") }}"
-                                alt="">
+                            <img src="{{ $new->image ? asset("images/$new->image") : asset("assets/img/logo.png") }}" alt="">
                             <h4><a href="{{ route('news.show', $new->id) }}">{{ $new->title }}</a></h4>
                             <p class="card-text">
                                 <i class="bx bxs-calendar"></i>
@@ -295,16 +286,17 @@
                             </p>
                         </div>
                         @endforeach
-
+        
                     </div><!-- End sidebar recent posts-->
-
+        
                 </div>
-            </div><!-- End News & Updates -->
-
-            <div class="card">
+            </div>
+            {{-- End News --}}
+        
+            <div class="card" id="positions">
                 <div class="card-body">
                     <h5 class="card-title">Positions</h5>
-
+        
                     <!-- Table with stripped rows -->
                     <table class="table table-striped">
                         <thead>
@@ -323,10 +315,9 @@
                         </tbody>
                     </table>
                     <!-- End Table with stripped rows -->
-
                 </div>
             </div>
-
+            {{-- End Positions --}}
 
         </div><!-- End Right side columns -->
 
